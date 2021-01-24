de8fe59e2800a97fe75125c66150dd85e3a5525a
vikhyat.prabhu <vikhyat.prabhu@gmail.com>
2021-01-24 14:45:12 +0530

    Add search functionality

--- src/main/java/com/ecommerce/bookstore/repository/BooksRepository.java
+++ src/main/java/com/ecommerce/bookstore/repository/BooksRepository.java
@@ -6,12 +6,14 @@ import com.ecommerce.bookstore.model.Book;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BooksRepository extends PagingAndSortingRepository<Book,Integer> {
 
+    @Query("SELECT b FROM Book b WHERE LOWER(b.title) LIKE LOWER(concat(?1, '%'))")
     Page<Book> findAllByTitle(String title, Pageable pageable);
 
 }
